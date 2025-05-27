4ba5b57501e86d7889ef5edd947785b13696c3f2
tom hikari <tomprescott1565@gmail.com>
2025-05-27 15:21:35 +0100

    Enhance App.js to retrieve and display lottery balance; update manager display text and add players state placeholder.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import lotteryContract from "./lottery";
 class App extends React.Component {
   state = {
     manager: "",
+    players: [],
+    balance: "",
   };
 
   async getManager() {
-    const manager = await lotteryContract.methods.manager().call();
-    console.log(manager);
-    this.setState({ manager });
+    Promise.all([
+      lotteryContract.methods.manager().call(),
+      web3.eth.getBalance(lotteryContract.options.address),
+      // lotteryContract.methods.players().call(),
+    ]).then((values) => {
+      console.log(values);
+      this.setState({
+        manager: values[0],
+        balance: values[1],
+        // players: values[1],
+      });
+    });
+
+    // this.setState({ manager, players, balance });
   }
 
   componentDidMount() {
@@ -22,11 +35,19 @@ class App extends React.Component {
     // console.log(web3);
     // web3.eth.getAccounts().then(console.log);
 
+    if (!this.state.manager) {
+      return <div>Connect to your metamask to continue 😀 </div>;
+    }
+
     return (
       <div className="App">
         <header>
-          <h1>Welcome to the Lottery</h1>
-          <p>The manager is {this.state.manager}</p>
+          <h1>Welcome to the Lottery 🥳</h1>
+          <p>The lottery is managed by {this.state.manager}</p>
+          <p>
+            The balance is {web3.utils.fromWei(this.state.balance, "ether")}{" "}
+            ether
+          </p>
         </header>
       </div>
     );
