7fcea42b45f1e6aaecf2c58139618d85b8d2db13
tom hikari <tomprescott1565@gmail.com>
2025-05-27 15:03:35 +0100

    Update dependencies in package-lock.json and package.json; upgrade web3 to version 4.16.0 and other related packages, add lottery contract integration in App.js with manager state retrieval.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import web3 from "./web3";
+import lotteryContract from "./lottery";
 
 class App extends React.Component {
+  state = {
+    manager: "",
+  };
+
+  async getManager() {
+    const manager = await lotteryContract.methods.manager().call();
+    console.log(manager);
+    this.setState({ manager });
+  }
+
+  componentDidMount() {
+    this.getManager();
+  }
   render() {
+    // console.log(web3);
+    // web3.eth.getAccounts().then(console.log);
+
     return (
       <div className="App">
         <header className="App-header">
---
+++ src/lottery.js
@@ -0,0 +1,60 @@
+import web3 from "./web3";
+
+const address = "0x706aa509781902C2Eb841F7685582bAF84a99560";
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "manager",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "players",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const lotteryContract = new web3.eth.Contract(abi, address);
+
+export default lotteryContract;
